a8339bc1562a668654b3fb5ce7e2922c050f7fbe
Rémi Carton <remi.carton@electric.ai>
2022-12-04 20:12:55 -0800

    day 4 🧹

--- advent/days/day04.py
+++ advent/days/day04.py
@@ -1,11 +1,34 @@
-import itertools as it
-from collections import deque
-from typing import Iterable, Iterator, TextIO, Tuple, TypeVar
+from dataclasses import dataclass
+from typing import TextIO
+
+
+@dataclass
+class Assignment:
+    start: int
+    end: int
+
+    @classmethod
+    def from_str(cls, s):
+        return Assignment(*map(int, s.split('-')))
+
+
+def fully_contains(a1: Assignment, a2: Assignment) -> bool:
+    return (a1.start <= a2.start and a1.end >= a2.end) or (a2.start <= a1.start and a2.end >= a1.end)
+
+
+def overlaps(a1: Assignment, a2: Assignment) -> bool:
+    return fully_contains(a1, a2) \
+           or (a1.start <= a2.start <= a1.end) \
+           or (a2.start <= a1.start <= a2.end)
 
 
 def first(input: TextIO) -> int:
-    return -1
+    pairs_str = [l.split(',') for l in input.read().split('\n') if l]
+    assignment_pairs = [(Assignment.from_str(a1), Assignment.from_str(a2)) for a1, a2 in pairs_str]
+    return sum(map(lambda x: int(fully_contains(*x)), assignment_pairs))
 
 
 def second(input: TextIO) -> int:
-    return -1
+    pairs_str = [l.split(',') for l in input.read().split('\n') if l]
+    assignment_pairs = [(Assignment.from_str(a1), Assignment.from_str(a2)) for a1, a2 in pairs_str]
+    return sum(map(lambda x: int(overlaps(*x)), assignment_pairs))
--- tests/test_day04.py
+++ tests/test_day04.py
@@ -1,14 +1,30 @@
 import io
+from advent.days.day04 import Assignment, first, fully_contains, second
+import pytest
 
-from advent.days.day04 import first, second
-
-data = """"""
+data = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
 data_io = io.StringIO(data)
 
 
+@pytest.mark.parametrize('a1, a2, expected', [
+    ('2-8', '3-7', True),
+    ('3-7', '2-8', True),
+    ('6-6', '4-6', True),
+    ('2-3', '4-5', False),
+])
+def test_fully_contains(a1, a2, expected):
+    a1, a2 = Assignment.from_str(a1), Assignment.from_str(a2)
+    assert fully_contains(a1, a2) == expected
+
+
 def test_first():
-    assert True is False
+    assert first(io.StringIO(data)) == 2
 
 
 def test_second():
-    assert True is False
+    assert second(io.StringIO(data)) == 4
