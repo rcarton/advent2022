a5bf26c999afc02ca2519a0eaebe27e98249a902
Rémi Carton <remi.carton@electric.ai>
2022-12-18 17:45:07 -0800

    wip_day17

--- advent/days/day17.py
+++ advent/days/day17.py
@@ -1,11 +1,193 @@
 import itertools as it
 from collections import deque
-from typing import Iterable, Iterator, TextIO, Tuple, TypeVar
+from dataclasses import dataclass
+from typing import Iterable, Iterator, List, Literal, TextIO, Tuple, TypeVar
+
+from advent.utils import binseq_to_int
+
+ROCKS_S = """####
+
+.#.
+###
+.#.
+
+..#
+..#
+###
+
+#
+#
+#
+#
+
+##
+##"""
+
+STARTING_LEFT_OFFSET = 2
+STARTING_VERT_OFFSET = 2
+CHAMBER_WIDTH = 7
+FULL_LINE = (1 << 7) - 1  # 127
+# 64: LIMIT_LEFT = 1 << (CHAMBER_WIDTH - 1)
+SHAPES = [[binseq_to_int(line.rstrip()
+                         .ljust(7, '.')
+                         .replace('#', '1')
+                         .replace('.', '0')) >> 2 for line in r.split('\n')]
+          for r in ROCKS_S.split('\n\n')]
+
+Move = Literal['<'] | Literal['>'] | Literal['v']
 
 
-def first(input: TextIO) -> int:
-    return -1
+@dataclass
+class Rock:
+    val: List[int]
+    # Height is relative compared to the top of the chamber, 0 means the bottom row of the rock overlaps the highest row
+    # of the chamber
+    height: int = 4
+
+
+Chamber = List[int]
+DEFAULT_CHAMBER = [(1 << CHAMBER_WIDTH) - 1]  # All 1s on the first row
+
+DEFAULT_ROW = (1 << (CHAMBER_WIDTH + 1)) | 1  # 257: 100000001
+
+
+def get_chamber_row_with_walls(c: Chamber, h: int) -> int:
+    """
+    Walls are used to check for moves with &:
+          |.###...|
+     ->   101110001
+    """
+    return (get_chamber_row(c, h) << 1) | DEFAULT_ROW
+
+
+def get_chamber_row(c: Chamber, h: int) -> int:
+    """
+          .###...
+     ->   0111000
+    """
+    # h == 0 means the top row of the chamber (== c[-1]
+    if h > 0:
+        return 0
+    return c[h - 1]
+
+
+def can_move(r: Rock, c: Chamber, m: Move) -> bool:
+    if m == '<':
+        return all(
+            get_chamber_row_with_walls(c, r.height + h) & (row << 2) == 0 for h, row in enumerate(reversed(r.val)))
+    if m == '>':
+        return all(
+            get_chamber_row_with_walls(c, r.height + h) & row == 0 for h, row in enumerate(reversed(r.val)))
+    if m == 'v':
+        return all(
+            get_chamber_row_with_walls(c, r.height + h - 1) & (row << 1) == 0 for h, row in enumerate(reversed(r.val)))
+
+    raise Exception('🐞')
+
+
+def move(r: Rock, c: Chamber, m: Move) -> bool:
+    if m == 'v' and r.height > 1:
+        r.height -= 1
+        return True
+
+    if can_move(r, c, m):
+        if m == '<':
+            r.val = [row << 1 for row in r.val]
+        elif m == '>':
+            r.val = [row >> 1 for row in r.val]
+        else:
+            r.height -= 1
+        return True
+
+    if m == 'v':
+        # We have to update the chamber before returning False
+        for h, row in enumerate(reversed(r.val)):
+            if r.height + h > 0:
+                # We append the row
+                c.append(row)
+            else:
+                # Merge the rows
+                c[r.height + h - 1] = get_chamber_row(c, r.height + h) | row
+    return False
+
+
+def repr_row(row: int, char='#'):
+    return bin(row)[2:].replace('1', char).replace('0', '.').rjust(CHAMBER_WIDTH, '.')
+
+
+def repr_chamber(c: Chamber, r: Rock | None) -> str:
+    if r is not None:
+        chamber_rows = c[1:] + [0] * (r.height - 1) + [0] * len(r.val)
+        rock_rows = [0] * (len(c) - 1) + [0] * (r.height - 1) + r.val[::-1]
+        shift = r.height
+        while shift <= 0 and len(chamber_rows) > 1:
+            shift += 1
+            rock_rows.pop(0)
+    else:
+        chamber_rows = c[1:]
+        rock_rows = [0] * len(chamber_rows)
+
+    o = ['+-------+\n']
+    for cr, rr in zip(chamber_rows, rock_rows):
+        if cr and rr:
+            cr_s = repr_row(cr)
+            rr_s = repr_row(rr, '@')
+            o_s = ''.join('#' if aa == '#' else '@' if bb == '@' else '.' for aa, bb in zip(cr_s, rr_s))
+            o += [f'|{o_s}|\n']
+        elif cr:
+            o += [f'|{repr_row(cr)}|\n']
+        else:
+            o += [f'|{repr_row(rr, "@")}|\n']
+    o = list(reversed(o))
+    return ''.join(o)
+
+
+def compress(c: Chamber, size: int = 500) -> int:
+    if (l := len(c)) < size:
+        return 0
+
+    for i in range(size):
+        r1, r2 = c[l - i - 1], c[l - i - 2]
+        if r1 | r2 == FULL_LINE:
+            # We can compress, everything before this row can be discarded
+            return l - i - 2
+
+
+def first(input: TextIO, max_rock=2023) -> int:
+    c = DEFAULT_CHAMBER
+    shapes = it.cycle(SHAPES)
+    jets = it.cycle(input.read().rstrip())
+
+    r = Rock(next(shapes))
+    m = next(jets)
+    rock_count = 1
+
+    compressed = 0
+    while rock_count < max_rock:
+
+        moved = move(r, c, m)
+
+        if not moved and m == 'v':
+            r = Rock(next(shapes))
+            rock_count += 1
+
+            # Try to compress the tower
+            if rock_count % 1000 == 0:
+                compression = compress(c)
+                if compression:
+                    c = c[compression:]
+                    compressed += compression
+                    # print(f'Compressed by {compression}')
+
+            if rock_count % 1_000_000 == 0:
+                print(f'rock_count={rock_count}, height={len(c) - 1 + compressed}')
+                print(repr_chamber(c, r))
+
+
+        m = next(jets) if m == 'v' else 'v'
+
+    return len(c) - 1 + compressed
 
 
 def second(input: TextIO) -> int:
-    return -1
+    return first(input, 1_000_000_000_000)
--- tests/test_day17.py
+++ tests/test_day17.py
@@ -1,14 +1,132 @@
 import io
+from typing import Optional, Tuple
 
-from advent.days.day17 import first, second
+import pytest
 
-data = """"""
+from advent.days.day17 import CHAMBER_WIDTH, Chamber, Rock, can_move, compress, first, move, repr_chamber, second
+from advent.utils import binseq_to_int
+
+data = """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
+"""
 data_io = io.StringIO(data)
 
+data_chamber = """
+|.....@.|
+|.....@.|
+|...@@@.|
+|..#....|
+|.###...|
+|..#....|
+|..####.|
++-------+
+"""
+
+data_chamber_w_merge = """
+|.....@.|
+|.....@.|
+|..#@@@.|
+|.###...|
+|..#....|
+|..####.|
++-------+
+"""
+
+def parse_case(i: str) -> Tuple[Chamber, Optional[Rock]]:
+    lines = i.lstrip().rstrip().splitlines()
+    lines = [l[1:-1] for l in lines[:-1]]
+    rock = []
+    rock_height = 1
+    c = []
+    for line in lines:
+        line = line.replace('.', '0')
+        rock_line = binseq_to_int(line.replace('@', '1').replace('#', '0'))
+        c_line = binseq_to_int(line.replace('@', '0').replace('#', '1'))
+        if rock_line:
+            rock.append(rock_line)
+        if c_line:
+            c.insert(0, c_line)
+        if not rock_line and not c_line:
+            rock_height += 1
+        if rock_line and c_line:
+            rock_height -= 1
+    c.insert(0, (1 << CHAMBER_WIDTH) - 1)
+    return c, Rock(rock, rock_height)
+
+
+def test_parse_case():
+    assert repr_chamber(*parse_case(data_chamber)) == data_chamber.lstrip()
+    assert repr_chamber(*parse_case(data_chamber_w_merge)) == data_chamber_w_merge.lstrip()
+
+def test_merge():
+    r = Rock([2, 2, 14], height=0)
+    c = [30, 16, 56, 16]
+    print(repr_chamber(c, r))
+
+    assert can_move(r, c, 'v') is False
+
+    assert move(r, c, 'v') is False
+    assert r.height == 0
+    assert(repr_chamber(c, None).strip()) == """
+|.....#.|
+|.....#.|
+|..####.|
+|.###...|
+|..#....|
++-------+
+""".strip()
+
+
+def test_move_bottom():
+    c, r = parse_case("""
+|...@@@@|
++-------+""")
+    move(r, c, '<')
+    assert(repr_chamber(c, r).strip()) == """
+|..@@@@.|
++-------+
+""".strip()
+
+    assert can_move(r, c, 'v') is False
+    move(r, c, 'v')
+    assert(repr_chamber(c, None).strip()) == """
+|..####.|
++-------+
+""".strip()
+
+
+def test_compress():
+    s = """
+|....@..|
+|....@..|
+|..@@@..|
+|.......|
+|.......|
+|.......|
+|..#....|
+|.###...|
+|..#....|
+|.####..|
+|######.|
+|....###|
+|....#..|
+|..#.#..|
+|..#.#..|
+|#####..|
+|..###..|
+|...#...|
+|..####.|
++-------+
+"""
+    c, r = parse_case(s)
+    result = compress(c)
+    assert result == 8
+    c = c[result:]
+    assert compress(c) == 0
+    print(repr_chamber(c, None))
 
 def test_first():
-    assert True is False
+    assert first(io.StringIO(data)) == 3068
 
 
 def test_second():
-    assert True is False
+    assert second(io.StringIO(data)) == 1514285714288
